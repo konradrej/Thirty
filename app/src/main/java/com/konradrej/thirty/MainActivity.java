3c4da2ffc149c4af57b40e5c5022d2aa506e2031
Konrad Rej <31326411+konradrej@users.noreply.github.com>
2021-06-20 04:36:26 +0200

    Finish main activity design

--- app/src/main/java/com/konradrej/thirty/MainActivity.java
+++ app/src/main/java/com/konradrej/thirty/MainActivity.java
@@ -3,6 +3,8 @@ package com.konradrej.thirty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> spinnerAdapater = ArrayAdapter.createFromResource(this, R.array.point_options, android.R.layout.simple_spinner_item);
+        spinnerAdapater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapater);
     }
 }
